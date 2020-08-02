eb751401b91e81b43658d25d5552da92722d2c86
Allespro <39953296+Allespro@users.noreply.github.com>
2020-08-02 03:18:59 +0300

    Update Arduino_NANO_sketch.ino

--- Arduino_NANO_sketch/Arduino_NANO_sketch.ino
+++ Arduino_NANO_sketch/Arduino_NANO_sketch.ino
@@ -1,32 +1,7 @@
-/*
-void setup() 
-{
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600); 
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-}
-void loop() {
-  if (Serial.available() > 0)
-  {
-    String ch;
-    ch = Serial.readString();
-    ch.trim();
-    if(ch == "DePapia"){
-        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-        Serial.print("DebugMeNow");
-    }
-  }
-  delay(20);                       // wait
-}
-*/
-
 void setup() 
 {
   Serial.begin(9600);
   Serial.print("password");
 }
-void loop() {
-  
-  //delay(10000);                 
+void loop() {          
 }
